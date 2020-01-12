c3cf4b92221a073b74f3b6a88c7fe2a55953c44b
Moroz Michael <michael08840884@gmail.com>
2020-01-12 13:00:05 +0200

    deleted folder

--- shaders/compute/utility/bloom.glsl
+++
@@ -1,6 +0,0 @@
-vec3 bloom_treshold(vec3 color, float exposure)
-{
-	color = clamp(color*exposure, 0, 100)/exposure;
-	vec3 mapped = Camera.bloomintensity*exposure*pow(color,vec3(2.));
-	return mapped;
-}
--- shaders/textures/readme.txt
+++
@@ -1,2 +0,0 @@
-Add textures here, they can be used in compute shaders.
-Only .png is supported
